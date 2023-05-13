d91c9d8face28a7dab673449a36d708611a7fb7a
Роман Юрьевич Троицкий <vzhyx@ya.ru>
2023-05-13 19:29:20 +0300

    feat: error page

---
+++ frontend/src/app/routes.tsx
@@ -0,0 +1,13 @@
+import {
+  createBrowserRouter
+} from 'react-router-dom'
+import Index from '@/pages/Index'
+import ErrorPage from '@/pages/ErrorPage'
+
+export const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Index />,
+    errorElement: <ErrorPage />,
+  },
+])
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@/src': path.resolve(__dirname, './src'),
+      '@/pages': path.resolve(__dirname, './src/pages'),
+      '@/shared/': path.resolve(__dirname, './src/shared'),
+      '@/icons': path.resolve(__dirname, './src/assets/icons/index.ts'),
+    },
+  },
 })
