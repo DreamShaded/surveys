2cfb49114b7db904e56963a0781e21c97d97e511
Роман Юрьевич Троицкий <vzhyx@ya.ru>
2023-05-16 00:40:13 +0300

    feat: vulnerabilities fix

--- backend/surveys_backend/settings.py
+++ backend/surveys_backend/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-6$t$8jbmtu1ov%v_-l=v7y_h$#a4c#l(9(3svmo_-^=apl)%ng'
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
