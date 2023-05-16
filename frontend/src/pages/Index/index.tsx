c140cd487a45831efdaa464b362cc6cea685ce14
Роман Юрьевич Троицкий <vzhyx@ya.ru>
2023-05-16 13:28:30 +0300

    feat: лейаут

--- frontend/src/pages/Index/index.tsx
+++ frontend/src/pages/Index/index.tsx
@@ -2,13 +2,17 @@ import { Layout, Space } from 'antd'
 
 const { Header, Footer, Content } = Layout
 
+const resetyStyle: React.CSSProperties = {
+  backgroundColor: 'white',
+}
+
 export default function Index(): JSX.Element {
   return (
     <Space direction='vertical' style={{ width: '100%' }} size={[0, 48]}>
       <Layout>
-        <Header>Header</Header>
-        <Content>Content</Content>
-        <Footer>Footer</Footer>
+        <Header style={resetyStyle}>Header</Header>
+        <Content style={resetyStyle}>Content</Content>
+        <Footer style={resetyStyle}>Footer</Footer>
       </Layout>
     </Space>
   )
