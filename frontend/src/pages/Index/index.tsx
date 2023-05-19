8a7a8bc7c95ee52642abc26c86b251863f135dc8
Роман Юрьевич Троицкий <vzhyx@ya.ru>
2023-05-19 21:01:32 +0300

    feat: добавил монтсеррат

--- frontend/src/pages/Index/index.tsx
+++ frontend/src/pages/Index/index.tsx
@@ -1,19 +1,7 @@
 import { Layout, Space } from 'antd'
 
-const { Header, Footer, Content } = Layout
-
-const resetyStyle: React.CSSProperties = {
-  backgroundColor: 'white',
-}
-
 export default function Index(): JSX.Element {
   return (
-    <Space direction='vertical' style={{ width: '100%' }} size={[0, 48]}>
-      <Layout>
-        <Header style={resetyStyle}>Header</Header>
-        <Content style={resetyStyle}>Content</Content>
-        <Footer style={resetyStyle}>Footer</Footer>
-      </Layout>
-    </Space>
+    <h1>Zdarov zaebal</h1>
   )
 }
